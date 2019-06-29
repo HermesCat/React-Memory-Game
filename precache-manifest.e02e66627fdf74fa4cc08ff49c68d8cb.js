0cc1d718bd82c204da6bc0dc241baedeb4864283
HermesCat <jppettis@comcast.net>
2019-06-29 10:55:25 -0400

    Updates

---
+++ precache-manifest.e02e66627fdf74fa4cc08ff49c68d8cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb83c6873f26888408aa61e8ecc5310",
+    "url": "/React-Memory-Game/index.html"
+  },
+  {
+    "revision": "b44da4180b0bc56b7d43",
+    "url": "/React-Memory-Game/static/css/main.2191c04d.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/React-Memory-Game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b44da4180b0bc56b7d43",
+    "url": "/React-Memory-Game/static/js/main.bcb997da.chunk.js"
+  },
+  {
+    "revision": "146a3181e76c06b70ec8",
+    "url": "/React-Memory-Game/static/js/runtime~main.39b1e4df.js"
+  }
+]);
